9c73de5cad44e30f412e3abdeb101b813c89c0fd
Patrick Einatz <patrick.einatz@udg.de>
2019-12-16 16:09:11 +0100

    +- markregexp, filterable memberlist

--- public/js/custom.js
+++ public/js/custom.js
@@ -22,3 +22,6 @@ $("#success-alert").fadeTo(2000, 500).slideUp(500, function(){
     $("#success-alert").slideUp(500);
 });
 
+$(document).ready(function() {
+    $("#content").markRegExp(/([@]|[#])([a-z])\w+/gmi);
+});
